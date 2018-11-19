d04dbe929c5035a58687e990569db64c33dc01a1
Kennie NG <jundywoo@gmail.com>
2018-11-19 23:52:48 +0800

    migration

--- aws-quiz-db-migration/src/main/java/ng/kennie/aws/quiz/migration/Application.java
+++ aws-quiz-db-migration/src/main/java/ng/kennie/aws/quiz/migration/Application.java
@@ -1,12 +1,14 @@
 package ng.kennie.aws.quiz.migration;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 import ng.kennie.aws.quiz.migration.config.QuizConfiguration;
 
 @ComponentScan("ng.kennie.aws.quiz.migration")
+@EnableAutoConfiguration
 @Import(QuizConfiguration.class)
 public class Application {
 
